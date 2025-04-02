2901a19e3d318dbe285aa434316d051b2ab9e33f
Mazaev Misha <maz06-11@yandex.ru>
2025-04-02 17:28:03 +0300

    исправлены ошибки

--- main.c
+++ main.c
@@ -2,73 +2,76 @@
 // Конвертер ASCII Hex <-> Bin
 // Мазаев М.И., 2025г.
 //------------------------------------------------------------------------------------------
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef enum
-{
-    ERR_INVALID_NUMBER_OF_PARAMETERS,
-    ERR_INVALID_PARAMETERS
-} ERROR_Status;
+#include <string.h>
 
 void printMenu()
 {
     printf("HEX <-> BIN converter\n");
-    printf("convertet. [OPTION] [FILE]\n");
-    printf(" -h - get help on using the utility\n");
-    printf(" --HexToBin -  convert HEX file to BIN file. After the parameter comes the /path/to/the/file/with/the/*.hex\n");
-    printf(" --BinToHex -  convert HEX file to BIN file. After the parameter comes the /path/to/the/file/with/the/*.hex\n");
+    printf("<util_name>.a [OPTION] [FILE]\n");
+    printf(" -h\t- get help on using the utility\n");
+    printf(" --HexToBin\t-  convert HEX file to BIN file. After the parameter comes the /path/to/the/file/with/the/*.hex\n");
+    printf(" --BinToHex\t-  convert BIN file to HEX file. After the parameter comes the /path/to/the/file/with/the/*.bin\n");
 }
 
-uint32_t convHexTobin(const char *inFile)
+void convHexTobin(const char *inFile)
 {
 
     FILE *input = fopen(inFile, "r");
     if (input == NULL)
         perror("Failed to open input file:");
 
-    char outFile[sizeof(inFile) + 4];
+    char outFile[FILENAME_MAX] = {0};
     strcat(outFile, inFile);
     strcat(outFile, ".bin");
     FILE *output = fopen(outFile, "wb");
-    if (input == NULL)
+    if (output == NULL)
         perror("Failed to open output file:");
 
-    unsigned char byte;
     char hex[3];
     hex[2] = '\0';
     hex[0] = fgetc(input);
     while (hex[0] != EOF)
     {
-        if ((hex[0] < '0' || hex[0] > '9') && (hex[0] < 'a' || hex[0] > 'f') && (hex[0] < 'a' || hex[0] > 'f'))
+        if ((hex[0] >'0' && hex[0] < '9') || (hex[0] > 'a' && hex[0] < 'f') || (hex[0] > 'a' && hex[0] < 'f')) 
         {
             hex[1] = fgetc(input);
-            byte = (unsigned char)strtol(hex, NULL, 16);
+            unsigned char byte = (unsigned char)strtol(hex, NULL, 16);
             fwrite(&byte, sizeof(byte), 1, output);
         }
+        hex[0] = fgetc(input);       
     }
-    return 0;
+
+    fclose(input);
+    fclose(output);
 }
 
-uint32_t convBinToHex(const char *inFile)
+void convBinToHex(const char *inFile)
 {
     FILE *input = fopen(inFile, "rb");
-    char outFile[sizeof(inFile) + 4];
+    if (input == NULL)
+        perror("Failed to open input file:");
+
+    char outFile[FILENAME_MAX] = {0};
     strcat(outFile, inFile);
     strcat(outFile, ".hex");
     FILE *output = fopen(outFile, "w");
+    if (output == NULL)
+        perror("Failed to open output file:");
 
-    unsigned char byte;
-    fread(&byte, sizeof(byte), 1, input);
+    int byte = fgetc(input); 
     while (byte != EOF)
     {
-        fprintf(output, "%02X", byte & 0xFF);
+        fprintf(output, "%02X", byte);
+        byte = fgetc(input);
     }
-    return 0;
+    
+    fclose(input);
+    fclose(output);
 }
 
-int main(int argc, char *argv[])
+void main(int argc, char *argv[])
 {
     if (argc < 2 || argc > 3)
     {
@@ -76,17 +79,22 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    if (strcmp(argv[1], "--help") || strcmp(argv[1], "-h"))
+    if (!(strcmp(argv[1], "--help") || strcmp(argv[1], "-h")))
         printMenu();
-    else if (strcmp(argv[1], "--HexToBin"))
-        if (strstr())
+    else if (!strcmp(argv[1], "--HexToBin"))
+    {
+        if (strstr(argv[2], ".hex") - argv[2] == strlen(argv[2]) - 4)
             convHexTobin(argv[2]);
-        else if (strcmp(argv[1], "--BinToHex"))
+    }
+    else if (!strcmp(argv[1], "--BinToHex"))
+    {
+        if (strstr(argv[2], ".bin") - argv[2] == strlen(argv[2]) - 4)
             convBinToHex(argv[2]);
-        else
-        {
-            printf("Error: Invalid parameter\nTo get help use -h or --help");
-            exit(EXIT_FAILURE);
-        }
-    return 0;
+    }
+    else
+    {
+        printf("Error: Invalid parameter\nTo get help use -h or --help");
+        exit(EXIT_FAILURE);
+    }
+    printf("1");
 }
